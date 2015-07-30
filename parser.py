9f75bfd267952cacbabc31b63f32c13193a32cbd
Damien Sarramalho <d.sarramalho@gmail.com>
2015-07-30 11:02:37 +0200

    Inserts the old parser in the python project

--- parser.py
+++ parser.py
@@ -4,9 +4,32 @@ from naoqi import ALProxy
 import actions
 import env
 
+understandableWords = ["sit", "stand", "say"]
+helloWords = ["hello", "hi", "hey"]
+priorityWords = ["now"]
+actionsArray = []
+
+
+def cut_sentence(sentence):
+    words = sentence.split(" ")
+    for word in words:
+        if word in priorityWords:
+            # get the last item and put it first
+            actionsArray.insert(0, actionsArray.pop())
+        if word in understandableWords or word in helloWords:
+            actionsArray.append(word)
+    return actionsArray
+
+
+def send_next():
+    if actionsArray:
+        # TODO send the action.
+        actions.do_action(actionsArray.pop(0))
+
 
 def parse(string):
     print 'Recognized: {}'.format(string)
     tts = ALProxy("ALTextToSpeech", env.nao_ip, env.nao_port)
-    actions.do_action(string)
+    cut_sentence(string)
+    actions.do_action(send_next)
     tts.say("What should I do now ?")
