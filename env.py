b2ac76e8dc9e3de774ba083f9f579d46fb8982a4
Gaël Bouchez <gael.bouchez@gmail.com>
2015-08-03 10:09:08 +0200

    Adds start and stop keywords

--- env.py
+++ env.py
@@ -1,3 +1,4 @@
 nao_ip = None
 nao_port = 9559
 global_rowing = False
+nao_listening = False
--- nao_parser.py
+++ nao_parser.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python
 
 import actions
+import env
 
-understandableWords = ["sit", "stand", "walk", "moonwalk", "row"]
+understandableWords = ["sit", "stand", "walk", "moonwalk", "row", "start", "stop"]
 helloWords = ["hello", "hi", "hey"]
 priorityWords = ["now"]
+startWords = ["start"]
+stopWords = ["stop"]
 actionsArray = []
 
 
@@ -12,10 +15,17 @@ def cut_sentence(sentence):
     words = sentence.split(" ")
     print(words)
     for word in words:
+        if word in startWords:
+            env.nao_listening = True
+        elif word in stopWords:
+            env.nao_listening = False
+
+        if not env.nao_listening:
+            continue
         if word in priorityWords:
             # get the last item and put it first
             actionsArray.insert(0, actionsArray.pop())
-        if word in understandableWords or word in helloWords:
+        elif word in understandableWords or word in helloWords:
             actionsArray.append(word)
     return actionsArray
 
